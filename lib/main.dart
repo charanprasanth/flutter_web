f4b973527ddada7127f21be6aa3d981c408ab810
Charan Prasanth L G <charanprasanth2001@gmail.com>
2021-07-09 15:35:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web/views/home/home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Open Sans'
+        ),
+      ),
+      home: HomeView(),
+    );
+  }
+}
+
